49aaf5fb24533dc06aa21de55343cb99613fc6df
Ignacio Alvarez <ignacio.alvarez@abcdin.cl>
2018-12-31 10:53:17 -0300

    add: se incluye boton que imprime json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,138 @@ export class AppComponent {
   title = 'libraries-export-app';
 
 
+  imprimeJson(){
+
+
+    var tablaSegundaPagina = {
+      has_contrato : "ha descripcion",
+      grower: "nombre_agricultor descripcion",
+      parent_lines_male:"lines males",
+      parent_lines_female:"lines female",
+      male_basic_seed:"male basic seed",
+      female_basic_seed:"female basic seed",
+      sowing_date_female:"sowing date female ",
+      sowing_date_male1:"sowing date male 1",
+      sowing_date_male2:"sowing date male 2"
+    };
+
+    var arraytablaSegundaPagina = [];
+    arraytablaSegundaPagina.push(tablaSegundaPagina); // Primera fila
+    arraytablaSegundaPagina.push(tablaSegundaPagina); // Segunda fila
+    arraytablaSegundaPagina.push(tablaSegundaPagina); // Tercera fila
+    arraytablaSegundaPagina.push(tablaSegundaPagina); // Cuarta fila
+    arraytablaSegundaPagina.push(tablaSegundaPagina); // Quinta fila
+    arraytablaSegundaPagina.push(tablaSegundaPagina); // Sexta fila
+    arraytablaSegundaPagina.push(tablaSegundaPagina); // Enésima fila
+
+
+    console.log("\nTABLA DE PRIMERA PÁGINA <= No es primordial esta tabla. Enfocarse en el informe más abajo\n");
+    for(var i=0;i<arraytablaSegundaPagina.length;i++){      
+      var arrayIn = arraytablaSegundaPagina[i];
+      for (var key in arrayIn){
+        if (arrayIn.hasOwnProperty(key)) {
+          //console.log("Valores: ",key,":",arrayIn[key]);
+        }
+      }
+
+    }
+
+
+
+
+
+    var informe = {
+      /*Esta es la tabla superior*/
+      nombre_variedad: "nombre_variedad descripcion",
+      nombre_agricultor:"nombre_agricultor descripcion",
+      locality: "locality descripcion",
+      ha: "ha descripcion",
+      flowering_start_female: "flowering_start_female descripcion",
+      flowering_start_male: "flowering_start_male descripcion",      
+      flowering_end_female: "flowering_end_female descripcion",
+      flowering_end_male: "flowering_end_male descripcion",      
+      crop_story:[/*Este array es de máximo 4 y mínimo zero*/
+        {
+          year: "2018",
+          detail:"Canola"
+        },
+        {
+          year: "2017",
+          detail:"Maravilla"
+        },
+        {
+          year: "2016",
+          detail:"Maiz"
+        },
+        {
+          year: "2015",
+          detail:"Frejol"
+        }
+      ],
+      isolation:"isolation descripcion",
+      trimming_female:"trimming_female descripcion",
+      trimming_male:"trimming_male descripcion",
+      seeding_date_female:"seeding_date_female descripcion",
+      seeding_date_male:"seeding_date_male descripcion",
+      bee_in_field:"bee_in_field descripcion",
+      estimated_yield_potential:"estimated_yield_potential descripcion",
+      average_plant:"average_plant descripcion",
+      /*Van 2 imágenes*/
+      images:[{
+        path: "http://localhost/imagen1.jpeg"
+      },{
+        path: "http://localhost/imagen2.jpeg"
+      }],
+      /*Tabla final*/
+      estado_general: "Buena",
+      estado_crecimiento: "Buena", 
+      estado_malezas: "Regular",
+      estado_fitosantiario:"Buena", 
+      humedad_suelo: "Buena"
+    };
+    /*Los posibles valores para los estado tienen diferentes colores*/  
+    /* 
+      Excelente : Azul
+      Buena: Verde
+      Regular: Amarillo
+      Mala: Rojo
+      Sin Especificar: Plomo
+      Baja : Plomo  /  Si es estado_malezas y Baja : Verde
+      Alta : Verde  /  Si es estado_malezas y Alta : Rojo
+    */ 
+
+    var array = [];
+    array.push(informe); // Primera página | El informe traerá otros datos por cada array
+    array.push(informe); // Segunda página | El informe traerá otros datos por cada array
+    array.push(informe); // Tercera página | El informe traerá otros datos por cada array
+    array.push(informe); // Cuarta página | El informe traerá otros datos por cada array
+    array.push(informe); // Quinta página | El informe traerá otros datos por cada array
+    array.push(informe); // Sexta página | El informe traerá otros datos por cada array
+    array.push(informe); // Enésima página | El informe traerá otros datos por cada array
+
+
+    for(var i=0;i<array.length;i++){
+      console.log("\nPágina "+(i+1)+"\n");
+      var arrayIn = array[i];
+      for (var key in arrayIn){
+        if (arrayIn.hasOwnProperty(key)) {
+          console.log("Valores: ",key,":",arrayIn[key]);
+        }
+      }
+
+    }
+    
+
+
+  }
+
 
   downloadPDFHTML(){
+
+
+    
+
+
 
   //variedadr html = "<html><head><meta charset='utf8'><title>SuitArt Business Card</title><style>#bg {            position: fixed;            z-index: -1;            top: 0;            left: 0;            width: 100%;                opacity: 0.5;                filter: alpha(opacity=50); /* For IE8 and earlier */            }            #bgicon {              position: fixed;              z-index: -1;              top: -0;              left: 0;              width: 10%;              }            .page {          text-align: center;          top: 320px;          padding-top: 100px;        }        td{            padding-top: 5px;            padding-left: 5px;            font-size: 10px;            font-family: Arial;        }        .imgClass{          width: 90%;        }        body{          font-size: 10px;          font-family: Arial;        }        </style>      </head>      <body >      <img id="bg" src="/home/ignacio/Escritorio/Proyectos Independientes/Curimapu/curimapu-api/resources/fondo.jpg" alt="Fondo" />      <img id="bgIcon" src="/home/ignacio/Escritorio/Proyectos Independientes/Curimapu/curimapu-api/resources/icon.jpg" alt="Fondo" />        <div class="page">        <h1>INFORME VISITA CULTIVO</h1>        <table border="1px solid" width="100%">        <tbody>        <tr>        <td width="50%">        <p><strong>Fecha:</strong> ${fec}</p>        <p>&nbsp;</p>        <p><strong>Agricultor:</strong>${contrato.id_agricultor.razon_social ? fnFormatoTexto(contrato.id_agricultor.razon_social) : '' } </p>        <p><strong>Telefono:</strong> ${contrato.id_agricultor.telefono[0] ? contrato.id_agricultor.telefono[0] : '' }</p>        <p><strong>E-Mail:&nbsp; </strong>${contrato.id_agricultor.email ? contrato.id_agricultor.email : '' }</p>        <p><strong>Especie:</strong> ${contrato.id_especie.nombre ? fnFormatoTexto(contrato.id_especie.nombre) : '' }</p>        </td>        <td width="50%">        <p><strong>Superficie:</strong> ${contrato.superficie ? fnFormatoTexto(contrato.superficie) : ''}</p>        <p><strong>Variedad:&nbsp; </strong>${variedad ? fnFormatoTexto(variedad) : '' }</p>        <p><strong>Potrero:</strong>${contrato.id_ficha.potrero ? fnFormatoTexto(contrato.id_ficha.potrero) : ''}</p>        </td>        </tr>        </tbody>        </table>        <p>&nbsp;</p>        <table border="1px" width="100%">        <tbody>        <tr>        <td width="50%">        <p><strong>Estado Fenol&oacute;gico:</strong>${visita.estado_fenologico}</p>        <p><strong>Estado malezas:</strong>${visita.estado_maleza}</p>        <p><strong>Cosecha: </strong>${visita.cosecha}</p>        </td>        <td width="50%">        <p><strong>Humedad:</strong> ${visita.humedad_suelo}</p>        <p><strong>Estado de crecimiento:</strong>${visita.estado_crecimiento}</p>            <p><strong>Estado fitosanitario:</strong> ${visita.estado_fitosantiario}</p>        <p><strong>Estado general del cultivo:</strong> ${visita.estado_general}</p>        </td>        </tr>        </tbody>        </table>        </div>        <br />        <h3>OBSERVACIONES GENERALES:</h3>       <p>        ${fnFormatoTexto(visita.observacion)}       </p>       <h3>RECOMENDACIONES:</h3>       <p>        ${fnFormatoTexto(visita.recomendaciones)}       </p>       ${img}      </body>    </html>";
 	  var html = "<html><body><h1>Hola Artiko</h1></body>    </html>";
